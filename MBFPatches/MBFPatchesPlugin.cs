c9957f3681b179b6fba871693777c0da01cce5bf
Fred Coughlin <fcoughlin@gmail.com>
2022-11-11 15:10:44 -0500

    Add project files.

---
+++ MBFPatches/MBFPatchesPlugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using Reactor.Utilities;
+using Reactor.Patches;
+using System;
+using MBFPatches.Patches;
+using Il2CppSystem.Data;
+
+namespace MBFPatches
+{
+
+
+    
+    [BepInPlugin(Id, "MBF - Patches", "1.0.0")]
+    [BepInProcess("Among Us.exe")]
+    
+    public partial class MBFPatchesPlugin : BasePlugin
+    {
+        
+        public const string Id = "me.fcoughlin.mbfpatches";
+        public Harmony Harmony { get; } = new(Id);
+        public static int optionsPage = 2;
+
+        public static ConfigEntry<bool> RandomSpawns { get; private set; }
+        public static ConfigEntry<bool> DynamicImpostors { get; private set; }
+        public static MBFPatchesPlugin Instance;
+
+        public override void Load()
+        {
+            Instance = this;
+            
+            
+            RandomSpawns = MBFPatchesPlugin.Instance.Config.Bind("Custom", "Random Spawns", true, "Enable Random Spawns on Mira, Polus and Skeld");
+            DynamicImpostors = MBFPatchesPlugin.Instance.Config.Bind("Custom", "Dynamic Impostors", true, "Enable dynamic impostor count, based on number of players.");
+
+            ReactorVersionShower.TextUpdated += (text) =>
+            {
+                int index = text.text.LastIndexOf('\n');
+                text.text = text.text.Insert(index == -1 ? text.text.Length - 1 : index, "   MBF Patches v1.0.0");
+            };
+
+            Harmony.PatchAll();
+        }
+
+    }
+
+}
---
+++ MBFPatches/Patches/GameStartManagerPatch.cs
@@ -0,0 +1,39 @@
+﻿using Hazel;
+using Reactor.Networking.Rpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBFPatches.Patches
+{
+    public class GameStartManagerPatch
+    {
+        public class GameStartManagerBeginGame
+        {
+            public static bool Prefix(GameStartManager __instance)
+            {
+                // Block game start if not everyone has the same mod version
+                bool continueStart = true;
+
+                if (AmongUsClient.Instance.AmHost)
+                {
+                    
+
+                    if (MBFPatchesPlugin.DynamicImpostors.Value)
+                    {
+                        var playerCount = GameData.Instance.PlayerCount;
+
+                        if (playerCount < 8) PlayerControl.GameOptions.NumImpostors = 1;
+                        else if (playerCount < 13) PlayerControl.GameOptions.NumImpostors = 2;
+                        else PlayerControl.GameOptions.NumImpostors = 3;
+                    }
+
+
+                }
+                return continueStart;
+            }
+        }
+    }
+}
